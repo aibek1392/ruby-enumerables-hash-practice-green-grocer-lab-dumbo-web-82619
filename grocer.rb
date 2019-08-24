bf3df50791cab9eed683a318316739dd892dfc2b
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-24 17:10:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,37 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
+ newcart = {}
+ items_name = cart.map do  |item|
+   item.keys.first
+   
+ end
+  cart.each do |item|
+           newcart[item.keys.first] = item[item.keys.first]
+       newcart[item.keys.first][:count] = items_name.select{|name| name == item.keys.first}.count
+      
+  #   if newcart.keys.include?(item.keys.first)
+  #     newcart[item.keys.first][:count] += 1
+  #   else
+  #     newcart[item.keys.first][:count] = 1
+  # end
+  # binding.pry
+  #   cart.each do |item|
+  #     newcart[i] = newcart[i] +=1 
+  #     return i if newcart[i]<1 
+  #   end
+  #     return nil
+  end
+  return newcart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  newbasket = {}
+  
+   coupons.each do |coupon|
+    coupon[:item][:coupon]
+     binding.pry
+      
+   end
 end
 
 def apply_clearance(cart)
