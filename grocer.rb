40d3372870f4d17d736a030966430005c5573f18
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-24 18:57:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,9 +47,23 @@ def apply_coupons(cart, coupons)
 
 
 def apply_clearance(cart)
-  # code here
+   cart.keys.each do |item|
+   if cart[item][:clearance]
+    cart[item][:price] = (cart[item][:price]*0.80).round(2)
+   end 
+ end 
+ cart
 end
 
 def checkout(cart, coupons)
-  # code here
+    cd_cart = consolidate_cart(cart)
+  cart_with_coupons_applied = apply_coupons(cd_cart, coupons)
+  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
+
+  total = 0.0
+  cart_with_discounts_applied.keys.each do |item|
+    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  end
+  total > 100.00 ? (total * 0.90).round : total
 end
+
